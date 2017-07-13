834e2cf9b8def2a7f4d843ad1378c6f7a1cb962d
OhYee <oyohyee@oyohyee.com>
2017-07-13 12:06:44 +0800

    其他数据

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 target/input/MessageWithCopiesCreateEvent.class target/input/MessageWithCopiesEventGenerator.class target/routing/SprayAndWaitRouterWithDiffCopies.class target/report/MyReport.class	:	src/input/MessageWithCopiesCreateEvent.java src/input/MessageWithCopiesEventGenerator.java src/routing/SprayAndWaitRouterWithDiffCopies.java src/report/MyReport.java	
-	javac -sourcepath src -d $targetdir -extdirs lib/ src/input/MessageWithCopiesCreateEvent.java src/input/MessageWithCopiesEventGenerator.java src/routing/SprayAndWaitRouterWithDiffCopies.java src/report/MyReport.java
+	javac -sourcepath src -d target -extdirs lib/ src/input/MessageWithCopiesCreateEvent.java src/input/MessageWithCopiesEventGenerator.java src/routing/SprayAndWaitRouterWithDiffCopies.java src/report/MyReport.java
 
 all: 
 	compile.sh
--- tools/calc.cpp
+++ tools/calc.cpp
@@ -1,27 +1,74 @@
-#include  <iostream>
+#include <iostream>
+#include <sstream>
+#include <string>
 using namespace std;
 
 struct Time {
     int Min, Max;
 };
 
-Time time[3];
-int node[3];
+Time time[4];
+int node[4];
+
+string Aline(string s)
+{
+    return s + "\n";
+}
+
+string toString(int temp)
+{
+    stringstream stream;
+    stream << temp;
+    return stream.str();
+}
+string toString(double s)
+{
+    int temp;
+    if ((int)(s * 10) % 10 >= 5)
+        temp = (int)s + 1;
+    else
+        temp = (int)s;
+    stringstream stream;
+    stream << temp;
+    return stream.str();
+}
 
 int main()
 {
     cout << "Input Node information" << endl;
-    for (int i = 0; i < 3; i++)
+    int sum = 0;
+    node[0] = 0;
+    for (int i = 1; i <= 3; i++) {
         cin >> node[i];
+        sum += node[i];
+    }
     cout << "Input Time information" << endl;
     for (int i = 0; i < 3; i++)
-        cin >> time[i].Min>> time[i].Max;
+        cin >> time[i].Min >> time[i].Max;
 
     cout << endl;
-    for (int i = 0; i < 3; i++) {
+
+    for (int i = 1; i <= 3; i++) {
         for (int j = 0; j < 3; j++) {
-            cout << "("<< (double)time[j].Min*60.0/node[i] <<","<< (double)time[j].Max*60.0/node[i] <<")\t";
+            cout << "(" << (double)time[j].Min * 60.0 / node[i] << "," << (double)time[j].Max * 60.0 / node[i] << ")\t";
         }
         cout << endl;
     }
+    cout << endl
+         << endl
+         << endl;
+
+    int pos = 1;
+    string s = "";
+    for (int i = 1; i <= 3; i++) {
+        s += Aline("Group" + toString(i) + ".nrofHosts=" + toString(node[i]));
+        for (int j = 0; j < 3; j++) {
+            //cout << "("<< (double)time[j].Min*60.0/node[i] <<","<< (double)time[j].Max*60.0/node[i] <<")\t";
+            s += Aline("Events" + toString(pos) + ".hosts=" + toString(node[i - 1]) + "," + toString(node[i]));
+            s += Aline("Events" + toString(pos) + ".interval=" + toString((double)time[j].Min * 60.0 / node[i]) + "," + toString((double)time[j].Max * 60.0 / node[i]));
+            s += Aline("Events" + toString(pos) + ".tohosts=0," + toString(sum));
+            pos++;
+        }
+    }
+    cout << s << endl;
 }
